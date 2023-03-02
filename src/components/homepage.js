60ae68ae92e9032fa75f7d40ae412387b68dbe07
Prem-rao <raopremwaghmare@gmail.com>
2023-03-02 18:11:21 +0530

    changed the component pages title

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
-import SearchForm from "../components/searchform";
+import Header from "./Header";
+import Footer from "./Footer";
+import SearchForm from "./searchform";
 import "../App.css";
 function Homepage() {
   return (
