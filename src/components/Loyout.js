a023cfe290e8c4f64c9c78aaf973809230790caf
Prem-rao <raopremwaghmare@gmail.com>
2023-03-15 18:17:03 +0530

    Update

--- src/components/Loyout.js
+++ src/components/Loyout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "./Header"
+import Footer from "./Footer"
 
 const Layout = () => {
     return (
