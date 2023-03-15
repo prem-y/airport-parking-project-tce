02c3e3a16a6e2cc5cac7fac94c300c71e14597bf
Prem-rao <raopremwaghmare@gmail.com>
2023-03-15 18:34:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
-import Layout from "./components/Loyout";
+import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import AirportAvailability from './pages/AirportAvailability';
 
