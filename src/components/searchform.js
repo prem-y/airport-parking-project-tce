5db69f853a5d810dc28a8385d2b9616f0b120f58
Prem-rao <raopremwaghmare@gmail.com>
2023-03-15 17:37:16 +0530

    updated .json file

--- src/components/searchform.js
+++ src/components/searchform.js
@@ -190,7 +190,7 @@ const SearchForm = () => {
                       </li>
                     );
                   })} */}
-                  {Loading ? <h1>Loading</h1> : null}
+                  {Loading ? <h1>loading..</h1> : null}
                   <AirportSuggetions
                     airports={filteredAirports}
                     selectAirport={selectAirport}
